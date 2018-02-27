42cbc236743dec2cd67b55d5af72c1698ca5d561
Rishi Karri <rishikarri1@gmail.com>
2018-02-26 22:19:41 -0500

    semantic and hardcoded card in place

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react';
+import { Card, Icon, Image, Label } from 'semantic-ui-react'
 import './App.css';
+
+const data = [
+
+  
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        hello
+      <div>
+
+      <Card
+        centered
+      >
+          <Image src='https://www.velvetjobs.com/assets/noavatar-profile.jpg' />
+          <Card.Content>
+            <Card.Header>
+              <Label centered size={'huge'} as='a' color='blue' image>                
+                Veronika
+                <Label.Detail>Friend</Label.Detail>
+              </Label>
+
+              {/*<div style={{marginTop: 20}}>&nbsp; </div>*/}              
+            </Card.Header>
+            <Card.Meta>
+              <span className='date'>
+                Joined in 2015
+              </span>
+            </Card.Meta>
+            <Card.Description>
+
+            <div>
+              <Label as='a' color='blue' image>                
+                email
+                <Label.Detail>veronika@gmail.com</Label.Detail>
+              </Label>
+            </div>
+
+
+              <div> 
+                <Label as='a' color='blue' image>                
+                  Phone Number
+                  <Label.Detail>878-888-8888</Label.Detail>
+                </Label>
+              </div>
+              <div> Phone Number: </div>
+              <div> Address: </div>
+              <div> Type: </div>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              22 Friends
+            </a>
+          </Card.Content>
+        </Card>
+
       </div>
     );
   }
